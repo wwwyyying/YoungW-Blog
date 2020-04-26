fce2e183de724b613d9b20034061478579570320
wwwyyying <iwangying_919@163.com>
2020-04-26 22:24:00 +0800

    stack and queue

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     //HTML & CSS: ['doc1', 'doc2', 'doc3'],
     JavaScript:['string','let-var-const','closure'],
-    数据结构与算法: ['array_2','linkedlist'],
+    数据结构与算法: ['array_2','linkedlist','stack'],
     //Features: ['mdx'],
   },
 };
