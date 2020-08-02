80e24a77b789f3d31a025b999f0696175bc10c35
wwwyyying <iwangying_919@163.com>
2020-08-02 17:18:57 +0800

    new原理

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,8 @@ module.exports = {
   someSidebar: {
     //HTML & CSS: ['doc1', 'doc2', 'doc3'],
     前端基础:['html','css'],
-    JavaScript:['string','let-var-const','closure'],
+    JavaScript基础:['string','let-var-const','closure'],
+    JavaScript原理:['new'],
     数据结构与算法: ['array_2','linkedlist','stack','hash','map','set','bubblesort','selectionsort','insertionsort','mergesort'],
     
     //Features: ['mdx'],
